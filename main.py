2ec4a95e45038a577ef131002704586616f67b9b
Nanunem <messad2.houamdi@gmail.com>
2021-10-09 10:13:39 +0200

    Modification de la prise en compte du fichier semaine.txt pour inclure le choix de tout télécharger ou une partie et le nombre de lignes de dialogue

--- main.py
+++ main.py
@@ -7,30 +7,42 @@ prefixe_exemple = "https://learn.dict.naver.com/dictPronunciation.dict?filePaths
 suffixe_exemple = "_study01_"
 
 
-class DateMining:
+class LectureFichier:
     """
     Object the defines the date containing the audio and text of the Naver lesson
     """
     def __init__(self, day_mining):
         self.day_mining = day_mining
 
-    def dates_to_consider(self):
-        correspondance = []
+    def lecture(self):
+        nb_dialogue = []
+        choix = []
         dates_demande = []
+        with open(self.day_mining, "r") as Entree:
+            for ligne in Entree:
+                if "#" in ligne :
+                    pass
+                else :
+                    ligne = ligne.strip()
+                    ligne = ligne.split()
+                    dates_demande.append(ligne[0].strip())
+                    choix.append(ligne[-1].strip())
+                    nb_dialogue.append(ligne[1].strip())
+        return dates_demande, choix, nb_dialogue
+
+    def dates_to_consider(self,dates):
+        correspondance = []
         ref_correspondance = "0574"
         ref_date = "2021-09-30"
         date_ref = ref_date.split("-")
-        with open(self.day_mining, "r") as Entree:
-            for ligne in Entree:
-                ligne = ligne.strip()
-                dates_demande.append(ligne)
-                ligne = ligne.split("-")
-                date1 = date(int(ligne[0]), int(ligne[1]), int(ligne[2]))
-                date2 = date(int(date_ref[0]), int(date_ref[1]), int(date_ref[2]))
-                diff = (date1 - date2).days
-                num_date = int(ref_correspondance) + diff
-                correspondance.append("0" + str(num_date))
-        return correspondance, dates_demande
+        for item in dates:
+            item = item.split("-")
+            date1 = date(int(item[0]), int(item[1]), int(item[2]))
+            date2 = date(int(date_ref[0]), int(date_ref[1]), int(date_ref[2]))
+            diff = (date1 - date2).days
+            num_date = int(ref_correspondance) + diff -1
+            correspondance.append("0" + str(num_date))
+        return correspondance
 
 
 class Audio:
@@ -60,48 +72,43 @@ class ChoixAction:
         self.demande = demande
 
     def download(self):
-        self.demande = self.demande.upper()
-
-        if "TOUT" in self.demande:                         # téléchargement de l'ensemble des dialogues et des exemples
-            for num, item in enumerate(correspondance):
-                for i in range(1, 7):
-                    audio = Audio(f"{prefixe_dialogue}{item}_0{str(i)}.mp3")
-                    audio.recherche_audio(f"dialogue_{dates_demande[num]}_{str(i)}")
+        for num, item in enumerate(self.demande) :
+            item = item.upper()
+            if "TOUT" in item:                         # téléchargement de l'ensemble des dialogues et des exemples
+                for i in range(1, int(nombre_dialogue[num])+1):
+                    audio = Audio(f"{prefixe_dialogue}{correspondance[num]}_0{str(i)}.mp3")
+                    audio.recherche_audio(f"dialogue_{dates[num]}_{str(i)}")
                 for i in range(1, 4):
-                    audio = Audio(f"{prefixe_exemple}{item}_study01_0{str(i)}.mp3")
-                    audio.recherche_audio(f"exemple__{dates_demande[num]}_{str(i)}")
+                    audio = Audio(f"{prefixe_exemple}{correspondance[num]}_study01_0{str(i)}.mp3")
+                    audio.recherche_audio(f"exemple__{dates[num]}_{str(i)}")
 
-        elif "DIAL" in self.demande:            # téléchargement des dialogues (tous les dialogues ou certaines phrases)
-            demande_precision = input("tout les dialogue ou quelques lignes (1 à 6) ? Indiquer \"TOUT\" ou une liste de chiffres")
-            if demande_precision == "TOUT":
-                for num, item in enumerate(correspondance):
-                    for i in range(1, 7):
-                        audio = Audio(f"{prefixe_dialogue}{item}_0{str(i)}.mp3")
-                        audio.recherche_audio(f"dialogue_{dates_demande[num]}_{str(i)}")
-            elif "1" in demande_precision or "2" in demande_precision or "3" in demande_precision or "4" in demande_precision or "5" in demande_precision or "6" in demande_precision:
-                pass
+            elif "DIAL" in item:            # téléchargement des dialogues (tous les dialogues ou certaines phrases)
+                for i in range(1, int(nombre_dialogue[num])+1):
+                    audio = Audio(f"{prefixe_dialogue}{correspondance[num]}_0{str(i)}.mp3")
+                    audio.recherche_audio(f"dialogue_{dates[num]}_{str(i)}")
+
+            elif "EXEM" in item:              # téléchargement des exemples (tous les exemples ou certaines phrases)
+                for i in range(1, 4):
+                    audio = Audio(f"{prefixe_exemple}{correspondance[num]}_study01_0{str(i)}.mp3")
+                    audio.recherche_audio(f"exemple__{dates[num]}_{str(i)}")
+
             else:
-                print("Erreur, merci d'indiquer \"TOUT\" ou une liste de chiffres")
-
-        elif "EXEM" in self.demande:              # téléchargement des exemples (tous les exemples ou certaines phrases)
-            pass
-
-        else:
-            print("Erreur, merci d'indiquer \"TOUT\", \"DIALogue\" ou \"EXEMple\"")
-            pass
-        return
-
+                print("Erreur, merci d'indiquer \"TOUT\", \"DIALogue\" ou \"EXEMple\"")
+                pass
+            return
 
 # Lecture du fichier semaine.txt définissant les dates à traiter
-fichier_dates = DateMining("semaine.txt")
-correspondance, dates_demande = fichier_dates.dates_to_consider()
+fichier_dates = LectureFichier("semaine.txt")
+dates, choix, nombre_dialogue = fichier_dates.lecture()
+correspondance = fichier_dates.dates_to_consider(dates)
 
 # Indication à l'utilisateur des dates traitées
 print("Les dates traitées sont :\n")
-for item in dates_demande:
+for item in dates:
     print(f"{item}\n")
 print("Modifiez le fichier \"semaine.txt\" si d'autres dates sont à traitées")
 
+
 # Choix des téléchargements à effectuer
-choix = ChoixAction(demande=input("Que télécharger ? \"TOUT\", \"DIALogue\" ou \"EXEMple\" "))
-choix.download()
+action = ChoixAction(demande=choix)
+action.download()
